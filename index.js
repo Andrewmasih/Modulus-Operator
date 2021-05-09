1f2ef96fac3046591d3ebd531d3ce8c68f00f615
Andrew Masih <armasih@outlook.com>
2021-05-09 22:06:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/* The result of this expression 
+is not how many time 10 is divided by 3 but
+is the remainder left after the division.
+If the divisor (on the right) divides evenly into the dividend (on the left)
+the answer will always be 0
+*/
+10 % 3;
